da86ea3e147918a9b3963dc590192ef0f2172b97
vaibhav322 <85068751+vaibhav322@users.noreply.github.com>
2023-08-17 21:55:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,17 +9,10 @@ function App() {
         <CurrentLocation />
       </div>
       <div className="footer-info">
-        <a href="https://www.htmlhints.com/article/how-to-create-toggle-switch/93">
-          Download Source Code
+         Developed by{" "}
+        <a target="_blank">
+          Vaibhav Choudhary
         </a>{" "}
-        | Developed by{" "}
-        <a target="_blank" href="https://www.gauravghai.dev/">
-          Gaurav Ghai
-        </a>{" "}
-        | Powered by{" "}
-        <a target="_blank" href="https://www.htmlhints.com/">
-          HTML HINTS
-        </a>
       </div>
     </React.Fragment>
   );
